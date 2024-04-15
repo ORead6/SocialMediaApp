7b01de79f9ac924e1b50c2c9a56cf8c1a007e471
ORead6 <owen-read@outlook.com>
2024-04-15 17:34:59 +0100

    Small Push

--- app/src/main/java/com/example/socialmediaapp/screens/InboxScreen.kt
+++ app/src/main/java/com/example/socialmediaapp/screens/InboxScreen.kt
@@ -1,31 +1,48 @@
 package com.example.socialmediaapp.screens
 
+import AutoCompleteTextField
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import com.example.socialmediaapp.components.myNavBar
 import com.example.socialmediaapp.signIn.UserData
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InboxScreen(
     userData: UserData?
 ) {
+
     Surface(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
-        Column (
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.White)
         ) {
             Text(text = "Inbox")
+
+
         }
 
     }
