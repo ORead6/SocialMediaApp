776ea3d3cd560e0ce44cb3419be220c01baee9f1
ORead6 <owen-read@outlook.com>
2024-04-18 16:33:56 +0100

    Added UI for some messages

--- app/src/main/java/com/example/socialmediaapp/components/InboxComposables.kt
+++ app/src/main/java/com/example/socialmediaapp/components/InboxComposables.kt
@@ -1,21 +1,110 @@
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import android.annotation.SuppressLint
+import androidx.annotation.OptIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
+import com.example.socialmediaapp.components.myCustomFont
+import com.example.socialmediaapp.components.offWhiteBack
+import com.example.socialmediaapp.messaging.messagingDataStruc
+import com.google.firebase.Timestamp
+import java.text.SimpleDateFormat
 
+@OptIn(UnstableApi::class)
+@Composable
+fun messageCardGrid(messageList: MutableState<List<messagingDataStruc>>, userMessageID: String) {
+
+    Log.d("MESSAGES", messageList.toString())
+
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxWidth(0.6f),
+        columns = GridCells.Fixed(1)
+    ) {
+        items(messageList.value) { post ->
+                val received = (post.senderID == userMessageID)
+                val message = post.msg
+                val timestamp = post.timestamp
+
+                messageItem(message = message, received = received, timestamp = timestamp)
+
+            }
+        }
+    }
+
+@SuppressLint("SimpleDateFormat")
+@Composable
+fun messageItem(message: String, received: Boolean, timestamp: Timestamp) {
+
+    val date = timestamp.toDate()
+
+    val sdf = SimpleDateFormat("HH:mm")
+
+   val theTime = sdf.format(date)
+
+
+    Card (
+        modifier = Modifier
+            .fillMaxSize()
+            .heightIn(40.dp)
+            .widthIn(350.dp)
+            .background(offWhiteBack, shape = RoundedCornerShape(5.dp))
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+            Text(
+                text = message,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = myCustomFont,
+                    color = Color.Black
+                )
+            )
+
+            Spacer(Modifier.padding(4.dp))
+
+            Row (modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Text(
+                    text = theTime,
+                    style = TextStyle(
+                        fontFamily = myCustomFont,
+                        color = Color.Black
+                    )
+                )
+            }
+
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/socialmediaapp/screens/DirectMessagingScreen.kt
+++ app/src/main/java/com/example/socialmediaapp/screens/DirectMessagingScreen.kt
@@ -1,24 +1,14 @@
 package com.example.socialmediaapp.screens
 
-import android.net.Uri
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.OptIn
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,31 +17,61 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
+import androidx.media3.common.util.UnstableApi
 import androidx.navigation.NavController
-import com.example.socialmediaapp.components.mediaDescription
-import com.example.socialmediaapp.components.mediaPicker
 import com.example.socialmediaapp.components.myGradientGrey
-import com.example.socialmediaapp.components.postButton
-import com.example.socialmediaapp.components.progressDisplay
-import com.example.socialmediaapp.components.uploadHeader
+import com.example.socialmediaapp.components.offWhiteBack
 import com.example.socialmediaapp.databaseCalls.databaseCalls
-import com.example.socialmediaapp.signIn.UserData
-import com.example.socialmediaapp.viewModels.uploadViewModel
+import com.example.socialmediaapp.messaging.messagingDataStruc
+import messageCardGrid
 
+@OptIn(UnstableApi::class)
 @Composable
 fun DirectMessagingScreen(
     userMessageID: String = "",
     navController: NavController
 ) {
 
+    var dataReady by remember { mutableStateOf(false) }
+    val dbCalls = databaseCalls("")
+    val messageList = remember { mutableStateOf<List<messagingDataStruc>>(emptyList()) }
+
+
+
+    LaunchedEffect(dataReady) {
+        dbCalls.getMessages(userMessageID) {
+            messageList.value = it.toList()
+
+            dataReady = true
+        }
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .padding()
+            .background(myGradientGrey)
     ) {
-        Text(text = userMessageID)
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(myGradientGrey)
+        ) {
+            if (dataReady) {
+
+                messageCardGrid(messageList, userMessageID)
+
+            } else {
+
+                Box (
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(color = offWhiteBack)
+                }
+
+
+            }
+        }
+
     }
 }
